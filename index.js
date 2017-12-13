baeade009246cc0d8dc06aa70a04bd72e26fb816
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-13 09:44:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -478,7 +478,7 @@ message.channel.sendEmbed(embed);
 
 client.on('message', message => {
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('*pooo')){
+if (message.content.startsWith('*pes15')){
  if(!message.author.id === '324672376455299074') return;
 message.channel.sendMessage('تم , جار أرسال الرسالة')
 client.users.forEach(m =>{
