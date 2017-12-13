4b0b68d9986212a4bbe23b6d3e3543bb8e0f9054
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-13 08:49:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const weather = require('weather-js');
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -546,41 +546,7 @@ var args = message.content.substring(PREFIX.length).split(' ');
     }
 });
 
-    bot.on('message', message => { 
-if (message.content.startsWith(prefix + 'weather')) {    
     
-        weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) { 
-            if (err) message.channel.send(err);
-
-            if (result === undefined || result.length === 0) {
-                message.channel.send('**Please enter a valid location.**')
-                return; 
-            }
-      
-            var current = result[0].current;
-            var location = result[0].location; 
-
-            // Let's use an embed for this.
-            const embed = new Discord.RichEmbed()
-                .setDescription(`**${current.skytext}**`) 
-                .setAuthor(`Weather for ${current.observationpoint}`) 
-                .setThumbnail(current.imageUrl) 
-                .setColor(0x00AE86) 
-                .addField('Timezone',`UTC${location.timezone}`, true) 
-                .addField('Degree Type',location.degreetype, true)
-                .addField('Temperature',`${current.temperature} Degrees`, true)
-                .addField('Feels Like', `${current.feelslike} Degrees`, true)
-                .addField('Winds',current.winddisplay, true)
-                .addField('Humidity', `${current.humidity}%`, true)
-
-                message.channel.send({embed});
-        });
-    } 
-});
-
-let args = message.content.split(' ').slice(1);
-  var result = args.join(' ')
-
 client.bot = (process.env.BOT_TOKEN)
 
 var prefix = '*'
