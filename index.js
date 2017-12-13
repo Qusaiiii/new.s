fb65e5cca449104e57e402c552d76b52d93a72b2
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-13 06:10:47 -0800

    Update index.js

--- index.js
+++ index.js
@@ -441,7 +441,7 @@ client.on("message", message => {
 
 
 client.on("ready", () => {
-  const Games = [`*help | *invite`]
+  const Games = [`*help | ${client.guilds.size}`]
   setInterval(() => { client.user.setGame(`${Games[Math.floor(Math.random() * Games.length)] }`) }, 10000)
 });
 
