a397f734a03d1e451a61d7a499cec2cb3345c39a
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-16 06:37:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag} !`);
-  client.user.setGame(`*help | in ${client.guilds.size} servers`,`www.twitch.tv/peery13`);
-});
-client.login(process.env.TOKEN);
 
 client.on('message', msg => {
   if (msg.content === 'pin31231231241sdg') {
@@ -654,6 +652,7 @@ break;
 }
 });    
 	
+client.user.setGame(`*help | in ${client.guilds.size} servers`,`www.twitch.tv/peery13`);
 	
 client.login(process.env.BOT_TOKEN);
 
