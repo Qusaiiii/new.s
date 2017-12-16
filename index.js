1be4337dc8f7fdb49de8a2794488a5865e921ec0
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-16 05:41:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
- client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+  client.user.setGame(`*help | ${client.guilds.size} servers`,`www.twitch.tv/zead11`);
 });
+
 
 client.on('message', msg => {
   if (msg.content === 'pin31231231241sdg') {
     msg.reply('Pong!');
   }
-});
-
-client.on("ready", () => {
-  const Games = [`*help | in ${client.guilds.size} servers`]
-  setInterval(() => { client.user.setGame(`${Games[Math.floor(Math.random() * Games.length)] }`) }, 10000)
 });
 
 client.on('message', message => {
