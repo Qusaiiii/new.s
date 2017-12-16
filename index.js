71a97d040015baddce47a2e28975b9a90ee42f13
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-16 07:54:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -674,23 +674,6 @@ if (message.content.startsWith("*cv")) {
 }
 });
 
-client.on('message', message => {
-    if (message.content.startsWith("*avatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
-});
-
 
 client.login(process.env.BOT_TOKEN);
 
