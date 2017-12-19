79bda5939717dab8dbed97b87eb83acdfba0f15a
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-19 07:42:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -719,7 +719,7 @@ client.on('guildMemberAdd', member => {
     .setDescription(`اهلا بك في السيرفر`)
     .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
     .setColor('GREEN')
-    .setFooter('TNT BOT', 'https://cdn.discordapp.com/avatars/390818524374761472/1860b4940801951560adf6866109393d.jpg?size=1024')
+    .setFooter('TNT BOT', 'https://images-ext-2.discordapp.net/external/0Q1jL6dCXH7mYjiOBsF-8t0QJMPejHinEmLh_kwSkaM/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/388700863893602304/d608a556d1bc36b1614f8cc1661957b9.png?width=225&height=225')
 
 var channel =member.guild.channels.find('name', 'wlc')
 if (!channel) return;
@@ -734,7 +734,7 @@ client.on('guildMemberRemove', member => {
     .setDescription(`الى اللقاء...`)
     .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
     .setColor('RED')
-    .setFooter(`TNT BOT`, 'https://cdn.discordapp.com/avatars/390818524374761472/1860b4940801951560adf6866109393d.jpg?size=1024')
+    .setFooter(`The King Bot`, '')
 
 var channel =member.guild.channels.find('name', 'wlc')
 if (!channel) return;
