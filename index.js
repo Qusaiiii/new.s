097ecd18a37fe3a071dd6b7ef22f340b0ec397ad
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-19 07:35:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -710,6 +710,38 @@ client.on('message', message => {
       message.channel.sendEmbed(embed);
     }
 });
+
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`عضو جديد`)
+    .setDescription(`اهلا بك في السيرفر`)
+    .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('GREEN')
+    .setFooter('TNT BOT', 'https://cdn.discordapp.com/avatars/390818524374761472/1860b4940801951560adf6866109393d.jpg?size=1024')
+
+var channel =member.guild.channels.find('name', 'wlc')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+client.on('guildMemberRemove', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`خرج عضو`)
+    .setDescription(`الى اللقاء...`)
+    .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('RED')
+    .setFooter(`TNT BOT`, 'https://cdn.discordapp.com/avatars/390818524374761472/1860b4940801951560adf6866109393d.jpg?size=1024')
+
+var channel =member.guild.channels.find('name', 'wlc')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+
 client.login(process.env.BOT_TOKEN);
 
 var prefix = '*'
