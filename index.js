2e191a559e6fbe6690b83f0141fe917f769ce588
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-25 16:56:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -519,43 +519,43 @@ msg.delete();
 });
 
 client.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
+  if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
   if (message.author.id !== "324672376455299074") return;
 
   
-  if (message.content.startsWith(PREFIX + 'setwatch')) {
+  if (message.content.startsWith(prefix + 'setwatch')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`Watch Now: **${argresult}`)
 } 
 
  
-  if (message.content.startsWith(PREFIX + 'setlis')) {
+  if (message.content.startsWith(prefix + 'setlis')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`LISTENING Now: **${argresult}`)
 } 
 
 
-if (message.content.startsWith(PREFIX + 'setname')) {
+if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
 } 
 
-if (message.content.startsWith(PREFIX + 'setavatar')) {
+if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(PREFIX + 'setstream')) {
+if (message.content.startsWith(prefix + 'setstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/KiNg66S");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}`)
 } 
-if (message.content.startsWith(PREFIX + 'setplay')) {
+if (message.content.startsWith(prefix + 'setplay')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
     message.channel.sendMessage(`Playing: **${argresult}`)
