ecdbce2dfd0b6230e9adf91de2cd6e02073b5d59
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-25 14:52:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on('message', message => {
       }
 });
 
-var prefix = "!!"
+
 client.on('message', message => {
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
@@ -21,7 +21,7 @@ client.on('message', message => {
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "kick") {
+  if (command == "!!kick") {
                if(!message.channel.guild) return message.reply('** This command only for servers**');
          
   if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
