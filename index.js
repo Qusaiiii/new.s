e40c34aaa1d35c76079bc505991b55bf159e7ffd
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-25 16:08:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -589,6 +589,17 @@ function isYoutube(str) {
      }
     });
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('!!bcall')){
+if(!message.author.id === '324672376455299074') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
